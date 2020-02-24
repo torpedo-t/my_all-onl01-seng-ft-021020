39380ee54ae4f8ae40c564549ddecc44385d0d9c
Thomas John Helin <thomashelin@yahoo.com>
2020-02-24 21:06:20 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,7 +3,7 @@ def my_all?(collection)
   block_return_values = []
   while i < collection.length
   block_return_values <<
-  yield(collecion[i])
+  yield(collection[i])
   i = i + 1
  end
  if block_return_values.include?(false)
